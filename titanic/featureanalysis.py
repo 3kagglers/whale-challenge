2727a8874b8dd4c48f106e6a81660fe8516a3d23
Iuri Ferreira <iuri_c_f@hotmail.com>
2019-06-04 19:05:21 +0200

    brings featureanalysis back

--- titanic/featureanalysis.py
+++ titanic/featureanalysis.py
@@ -2,147 +2,179 @@
 Feature analysis.
 """
 
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
+from collections import Counter
+
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 class FeatureAnalysis:
     """
     Class to analyse the features from titanic challenge.
     Call ... to work on input features, then call
     process_test_dataset to work on test data.
-    get_correlation_numericalvalues will create graphs that show the correlation
-     among features.
+    get_correlation_numerical_values will create graphs that show the
+    correlation among features.
     """
-    #remake definition at the end
 
-    #def __init__(self):
-
-
-    def get_correlation_numericalvalues(self, dataframe, numerical_tag) -> None:
+    def __init__(self, dataframe, show_plots=True):
         """
-        Receives the dataframe and creates the graphs to analyse correlation
-        among numerical features.
+        Receives a dataframe to be worked.
 
         :dataframe: pd.DataFrame
-        :numerical_tag: indexes to be considered
-        :return: None
+        :show_plots: boolean : (optional) allows user to choose whether to plot
+                               diagrams or not.
         """
+        self.dataframe = dataframe
+        self.show_plots = show_plots
         sns.set(style='white', context='notebook', palette='deep')
 
-        # Correlation matrix between numerical values (SibSp Parch Age and Fare
-        # values) and Survived
-        g = sns.heatmap(
-            dataframe[numerical_tag].corr(),
+    def get_correlation_numerical_values(self, indexes) -> None:
+        """
+        Creates the graphs to analyse correlation among numerical features.
+        First in list indexes will be the one to have correlation checked
+        against others (you maybe want it to be the output)
+
+        :indexes: List[str] : dataframe indexes to be considered
+        :return: None
+        """
+        sns.heatmap(
+            self.dataframe[indexes].corr(),
             annot=True,
             fmt=".2f",
             cmap="coolwarm")
-        plt.show()
+        if self.show_plots: plt.show()
 
-        # Explore SibSp feature vs Survived
-        self.explore_feature_output(dataframe, "SibSp", "Survived")
-        plt.show()
-
-        # Explore Parch feature vs Survived
-        self.explore_feature_output(dataframe, "Parch", "Survived")
-        plt.show()
-
-        # Explore Age vs Survived
-        g = sns.FacetGrid(dataframe, col='Survived')
-        g = g.map(sns.distplot, "Age")
-        plt.show()
-
-        # Explore Age distibution
-        g = sns.kdeplot(
-            dataframe["Age"][
-                (dataframe["Survived"] == 0) & (dataframe["Age"].notnull())],
-            color="Red",
-            shade=True)
-        g = sns.kdeplot(
-            dataframe["Age"][
-                (dataframe["Survived"] == 1) & (dataframe["Age"].notnull())],
-            ax=g,
-            color="Blue",
-            shade=True)
-        g.set_xlabel("Age")
-        g.set_ylabel("Frequency")
-        g = g.legend(["Not Survived", "Survived"])
-        plt.show()
-
-        # Explore Fare distribution
-        g = sns.distplot(
-            dataframe["Fare"],
-            color="m",
-            label="Skewness : %.2f" % (dataframe["Fare"].skew()))
-        g = g.legend(loc="best")
-        plt.show()
-
-    def analyse_categoricalvalues(self, dataframe) -> None:
+    def distribution_plot_against_categorical(self, index_one, index_two):
         """
-        Receives the dataframe and creates the graphs to analyse the categorical
-        features.
+        Plots index_two against caregories of index_one, e.g. distribution of
+        index_two for index_one == 0 and dist of index_two for index_one == 1.
+        index_one should be categorical and 1 should be numerical.
 
-        :dataframe: pd.DataFrame
+        :index_one: str : "Categorical" feature
+        :index_two: str : Numerical feature
+        """
+        # Explore Age vs Survived
+        tmp = sns.FacetGrid(self.dataframe, col=index_one)
+        tmp.map(sns.distplot, index_two)
+        if self.show_plots: plt.show()
+
+    def plot_against_true_false_var(self, index, tf_index):
+        """
+        Creates a graph to plot one feature against a boolean feature.
+        Boolean is matched using "dataframe[tf_index] == [0 (or) 1]".
+
+        FIXME: maybe use bool(feature) / not bool(feature) to evaluate instead
+
+        :index: str : index of dataframe to be plotted against True/F var
+        :tf_index: str : index of dataframe True/False variable for separation
         :return: None
         """
-        sns.set(style='white', context='notebook', palette='deep')
+        # Explore Age distibution
+        tmp = sns.kdeplot(
+            self.dataframe[index][(
+                self.dataframe[tf_index] == 0)
+                & (self.dataframe[index].notnull())],
+            color="Red",
+            shade=True)
+        tmp = sns.kdeplot(
+            self.dataframe[index][(
+                self.dataframe[tf_index] == 1)
+                & (self.dataframe[index].notnull())],
+            ax=tmp,
+            color="Blue",
+            shade=True)
+        tmp.set_xlabel(index)
+        tmp.set_ylabel("Frequency")
+        tmp.legend(["Not " + tf_index, index])
+        if self.show_plots: plt.show()
 
-        # Explore Sex vc Survived
-        g = sns.barplot(x="Sex", y="Survived", data=dataframe)
-        g = g.set_ylabel("Survival Probability")
-        plt.show()
+    def explore_distribution_plot(self, index):
+        """
+        Calls distribution plot for data skewness.
+        :index: str : index of dataframe to be plotted.
+        """
+        tmp = sns.distplot(
+            self.dataframe[index],
+            color="m",
+            label="Skewness : %.2f"%(self.dataframe[index].skew()))
+        tmp.legend(loc="best")
+        if self.show_plots: plt.show()
 
-        # Explore Pclass vs Survived
-        self.explore_feature_output(dataframe, "Pclass", "Survived")
-        plt.show()
+    def categorical_plot_frequency(self, index_one, index_two):
+        """
+        Plots count of index_one vs index_two.
+        :index_one: str : Variable to be plotted against index_two
+        :index_two: str : Variable to be plotted against index_one
+        """
+        tmp = sns.barplot(x=index_one, y=index_two, data=self.dataframe)
+        tmp.set_ylabel("Probability of " + index_two)
+        if self.show_plots: plt.show()
 
-        # Explore Pclass vs Survived by Sex
-        g = sns.catplot(
-            x="Pclass",
-            y="Survived",
-            hue="Sex",
-            data=dataframe,
+    def categorical_plot_three_vars(self, index_one, index_two, index_three):
+        """
+        Plots count of index_one vs index_two by (hue) index_three.
+        :index_one: str : Feature to be plotted
+        :index_two: str : Feature to be plotted
+        :index_three: str : Feature to be plotted as hue
+        """
+        tmp = sns.catplot(
+            x=index_one,
+            y=index_two,
+            hue=index_three,
+            data=self.dataframe,
             height=6,
             kind="bar",
             palette="muted")
-        g.despine(left=True)
-        g = g.set_ylabels("survival probability")
-        plt.show()
+        tmp.despine(left=True)
+        tmp.set_ylabels("probability of " + index_two)
+        if self.show_plots: plt.show()
 
-        # Explore Embarked vs Survived
-        self.explore_feature_output(dataframe, "Embarked", "Survived")
-        plt.show()
-
-        # Explore Pclass vs Embarked
-        g = sns.catplot(
-            "Pclass",
-            col="Embarked",
-            data=dataframe,
+    def categorical_plot_two_vars(self, index_one, index_two):
+        """
+        Plots count of index_one vs index_two.
+        :index_one: str : Variable to be plotted against index_two
+        :index_two: str : Variable to be plotted against index_one
+        """
+        tmp = sns.catplot(
+            index_one,
+            col=index_two,
+            data=self.dataframe,
             height=6,
             kind="count",
             palette="muted")
-        g.despine(left=True)
-        g = g.set_ylabels("Count")
-        plt.show()
+        tmp.despine(left=True)
+        tmp.set_ylabels("Count")
+        if self.show_plots: plt.show()
 
-    @staticmethod
-    def explore_feature_output(dataframe, feature, output) -> None:
+    def explore_feature_output(self, feature, output) -> None:
         """
         Creates graph to explore the influence of one feature on the output or
         the correlation among two features.
 
-        :dataframe: pd.DataFrame
         :feature: input feature
         :output: output
         :return: None
         """
-        g = sns.catplot(
+        tmp = sns.catplot(
             x=feature,
             y=output,
-            data=dataframe,
+            data=self.dataframe,
             kind="bar",
             height=6,
             palette="muted")
-        g.despine(left=True)
-        g = g.set_ylabels("Survival Probability")
-        plt.show()
+        tmp.despine(left=True)
+        tmp.set_ylabels("Survival Probability")
+        if self.show_plots: plt.show()
