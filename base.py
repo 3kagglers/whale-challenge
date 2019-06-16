05b682f3f38ae774c53a19fe9403e4d432fef990
Iuri Ferreira <iuri_c_f@hotmail.com>
2019-06-16 10:43:43 +0200

    change to base.py max line length

--- base.py
+++ base.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 import preprocessor
 
-# ============================================================================ #
+# =========================================================================== #
 # loading and checking data
-# ============================================================================ #
+# =========================================================================== #
 
 # Load data
 # Probably use pd.read_csv or Preprocessor class
@@ -18,9 +18,9 @@ import preprocessor
 # Merge them
 # Apply operations to both datasets
 
-# ============================================================================ #
+# =========================================================================== #
 # Feature analysis
-# ============================================================================ #
+# =========================================================================== #
 
 # Verify correlation mong features and correlation between each feature and the
 # output, this tells whether a feature is important or not
@@ -28,38 +28,38 @@ import preprocessor
 # Analyze numerical values
 # Analyze categorical values
 
-# ============================================================================ #
+# =========================================================================== #
 # Filling missing values
-# ============================================================================ #
+# =========================================================================== #
 
 # Fill missing values with a relevant, thought-out value
 
-# ============================================================================ #
+# =========================================================================== #
 # Feature engineering
-# ============================================================================ #
+# =========================================================================== #
 
-# Try and create new features from others, e.g. check whether two features, when
-# multiplied, return some interesting pattern
+# Try and create new features from others, e.g. check whether two features
+# when multiplied, return some interesting pattern
 
-# ============================================================================ #
+# =========================================================================== #
 # Changing feature representation
-# ============================================================================ #
+# =========================================================================== #
 
-# Use one-hot-encoding/binary encoding to turn categorical features into numeric
-# features to be analyzed without pounding.
+# Use one-hot-encoding/binary encoding to turn categorical features into
+# numeric features to be analyzed without pounding.
 
-# ============================================================================ #
+# =========================================================================== #
 # Modeling
-# ============================================================================ #
+# =========================================================================== #
 
 # Define model(s), NN, DT, SVM,...
 # Train model(s)
 # Plot learning curve
 # Ckeck cross-validation
-# Tune hyper-parameters, strategies for this are e.g. gridSearch or genetic alg.
+# Tune hyper-parameters, strategies for this are e.g. gridSearch or genetic alg
 
-# ============================================================================ #
+# =========================================================================== #
 # Prediction
-# ============================================================================ #
+# =========================================================================== #
 
 # Test it out.
